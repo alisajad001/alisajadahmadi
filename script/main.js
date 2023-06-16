bd6032249274715cff45026d5521e6c0270ef471
Ali Sajad Ahmadi <forwebonly0@gmail.com>
2023-06-17 00:05:00 +0430

    Added theme toggler

--- script/main.js
+++ script/main.js
@@ -1,7 +1,11 @@
 const burgerWrapper = document.querySelector('.burger-wrapper');
 const navbar = document.querySelector('.nav');
+const sunIcon = document.querySelector('.fa-sun');
+const body = document.body;
+
 let menuOpen = false;
 
+// Opens and closes navabar
 burgerWrapper.addEventListener('click', () => {
     if (!menuOpen) {
         burgerWrapper.classList.add('active');
@@ -13,3 +17,23 @@ burgerWrapper.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+// Toggles between dark mode and light mode
+sunIcon.addEventListener('click', () => {
+    body.classList.toggle('darkMode');
+
+    if (body.className.includes('darkMode')) {
+        localStorage.setItem('theme', 'dark' )
+    } else {
+        localStorage.setItem('theme', 'light')
+    };
+});
+
+// Updates the theme of the page based on user local storage
+window.addEventListener('load', () => {
+    let localStorageTheme = localStorage.getItem('theme');
+
+    if (localStorageTheme === 'dark') {
+        body.classList.add('darkMode')
+    }
+})
